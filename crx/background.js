10ad8d649dc87faeccc96a66e17f53e651086c19
netnr <netnr@netnr@.com>
2018-05-27 18:20:16 +0800

    update

--- crx/background.js
+++ crx/background.js
@@ -1,11 +1,10 @@
-//chrome.runtime.onInstalled.addListener(function() {
 chrome.contextMenus.create({
-    title: "VIP解析",
+    title: "VIP视频解析",
     contexts: ["page"],
-    documentUrlPatterns: ["*://*.mgtv.com/*", "*://*.letv.com/*", "*://*.youku.com/*", "*://*.iqiyi.com/*", "*://*.v.qq.com/*", "*://*.tudou.com/*", "*://*.wasu.cn/*", "*://*.ku6.com/*", "*://*.56.com/*", "*://*.tv.sohu.com/*", "*://*.film.sohu.com/*", "*://*.1905.com/*", "*://*.pptv.com/*", "*://*.baofeng.com/*", "*://*.bilibili.com/*", "*://*.fun.tv/*", "*://*.6.cn/*"],//
+    documentUrlPatterns: ["*://*.mgtv.com/*", "*://*.letv.com/*", "*://*.youku.com/*", "*://*.iqiyi.com/*", "*://*.v.qq.com/*", "*://*.tudou.com/*", "*://*.wasu.cn/*", "*://*.ku6.com/*", "*://*.56.com/*", "*://*.tv.sohu.com/*", "*://*.film.sohu.com/*", "*://*.1905.com/*", "*://*.pptv.com/*", "*://*.baofeng.com/*", "*://*.bilibili.com/*", "*://*.fun.tv/*", "*://*.6.cn/*"],
     targetUrlPatterns: ["http://*/*"],
     onclick: function (b) {
-        burl = "http://v.netnr.com/?url=" + encodeURIComponent(b.pageUrl);
+        burl = "https://v.netnr.com/?url=" + encodeURIComponent(b.pageUrl);
         chrome.tabs.query({
             active: true,
             currentWindow: true
@@ -18,7 +17,6 @@ chrome.contextMenus.create({
 });
 chrome.browserAction.onClicked.addListener(function (b) {
     chrome.tabs.create({
-        url: 'http://v.netnr.com'
+        url: 'https://v.netnr.com'
     })
-})
-//});
+});
