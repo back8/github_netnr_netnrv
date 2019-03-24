d12f0a623342a7f2dcabeb5f894c4784f0fa6780
netnr <netnr@netnr.com>
2019-03-24 17:45:38 +0800

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //线路
 var vipSource = [
+    "https://player.baodai.org/ipsign/player.php?v=",
     "https://jx.dy-jx.com/?url=",
     "https://jiexi.071811.cc/jx2.php?url=",
-    "https://player.baodai.org/ipsign/player.php?v=",
     "https://api.bbbbbb.me/v.php?url="
 ];
 
