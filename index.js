587f95e689ac7bc1b51ecabe5aded4defb94be44
netnr <netnr@netnr.com>
2019-09-01 12:46:35 +0800

    update

--- index.js
+++ index.js
@@ -13,52 +13,37 @@ function init() {
     for (; i < len;) {
         htm.push('<option value="' + vipSource[i++] + '">线路 ' + i + '</option>');
     }
-    $('#seVtype').html(htm.join(''));
+    document.getElementById('seVtype').innerHTML = htm.join('');
     //带参播放
     var pars = location.search.toLowerCase().substring(1).split('&');
     for (var i = 0; i < pars.length; i++) {
         var pi = pars[i];
         if (pi.indexOf('url=') >= 0) {
-            $('#txtVipUri').val(decodeURIComponent(pi.substring(4)));
+            document.getElementById('txtVipUri').value = decodeURIComponent(pi.substring(4));
             break;
         }
     }
-    $('#btnPlay')[0].click();
+    document.getElementById('btnPlay').click();
 }
 
 //切换源
-$('#seVtype').change(function () {
-    $('#btnPlay')[0].click();
-});
+document.getElementById("seVtype").onchange = function () {
+    document.getElementById('btnPlay').click();
+}
 
-//播放
-$('#txtVipUri').keydown(function (e) {
-    e = e || window.event;
-    var key = e.keyCode || e.which || e.charCode;
-    if (key == 13) {
-        $('#btnPlay')[0].click();
+document.getElementById('btnPlay').onclick = function () {
+    var uri = document.getElementById("txtVipUri").value;
+    if (uri.length > 10) {
+        document.getElementById('inetnr').src = document.getElementById('seVtype').value + document.getElementById('txtVipUri').value;
     }
-});
-$('#btnPlay').click(function () {
-    if ($('#txtVipUri').val().length > 10) {
-        $('#inetnr')[0].src = $('#seVtype').val() + $('#txtVipUri').val();
-    }
-});
+}
 
-//全屏
-$('#aGoFull').click(function () {
-    var src = $('#inetnr')[0].src;
-    if (src.length > 15) {
-        location.href = src;
-    } else {
-        alert("请先粘贴VIP视频链接");
-    }
-});
+window.onload = window.onresize = function () {
+    var iv = document.getElementById('inetnr');
+    var vh = document.documentElement.clientHeight - iv.getBoundingClientRect().top - 15;
+    iv.style.height = vh + "px";
+}
 
-//自适应高度
-$(window).on('load resize', function () {
-    var ifn = $('#inetnr'), ch = $(this).height() - ifn.offset().top;
-    ifn.css('height', ch - 45);
-});
-
-init();
+setTimeout(function () {
+    init();
+}, 10)
